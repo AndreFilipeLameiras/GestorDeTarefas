055c59b3f27e21f9be7d99b74634c5723e508f0e
Carlos Cruz <1701062@sal.ipg.pt>
2021-11-10 09:39:10 +0000

    Criado a pagina Termos e Condições

--- GestorDeTarefas/Controllers/HomeController.cs
+++ GestorDeTarefas/Controllers/HomeController.cs
@@ -45,6 +45,10 @@ namespace GestorDeTarefas.Controllers
         {
             return View();
         }
+        public IActionResult TermosCondicoes()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
