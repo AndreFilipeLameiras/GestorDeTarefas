c7b76980feedd7dea7964c4cce0c362720ef22ed
Lameiras <afplameiras@gmail.com>
2021-11-12 11:16:32 +0000

    Corrigido erro na base de dados

---
+++ GestorDeTarefas/Data/QuadrosMigration/20211112101824_initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestorDeTarefas.Data.QuadrosMigration
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quadros",
+                columns: table => new
+                {
+                    QuadrosId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quadros", x => x.QuadrosId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quadros");
+        }
+    }
+}
