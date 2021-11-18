e2488b7738d3d757e5df0fdd3dbe41ae3df9ab28
Lameiras <afplameiras@gmail.com>
2021-11-18 20:35:53 +0000

    Criada a class representada pelos Colaboradores

---
+++ GestorDeTarefas/Models/Colaborador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.Models
+{
+    public class Colaborador
+    {
+        public int ColaboradorId { get; set; }
+        [Required]
+        [StringLength(256)]
+        public string Name { get; set; }
+    }
+}
