9ebffe488fd7c9500d2abbbd35980b97ffa8b84a
Carlos Cruz <1701062@sal.ipg.pt>
2021-11-23 17:56:15 +0000

    Criada a lista de tarefas

---
+++ GestorDeTarefas/ViewModels/TarefaListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GestorDeTarefas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.ViewModels
+{
+    public class TarefaListViewModel
+    {
+      
+            public IEnumerable<Tarefas> Tarefass { get; set; }
+            public PagingInfo PagingInfo { get; set; }
+        
+    }
+}
