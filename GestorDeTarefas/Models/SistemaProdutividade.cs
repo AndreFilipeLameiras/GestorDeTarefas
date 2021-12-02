83511d16cb0bce81a023de516b7d35eef9f94132
Lameiras <afplameiras@gmail.com>
2021-12-02 19:45:20 +0000

    Criaçao da class de um modelo  SistemaProdutividade

---
+++ GestorDeTarefas/Models/SistemaProdutividade.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.Models
+{
+    public class SistemaProdutividade
+    {
+        public int SistemaProdutividadeId { get; set; }
+
+
+        [Required]
+        [StringLength(256)]
+        public string Nome { get; set; }
+
+        [StringLength(256)]
+        public string O_que_fazer { get; set; }
+
+        [StringLength(256)]
+        public string Estamos_a_fazer { get; set; }
+
+        [StringLength(256)]
+        public string Concluido { get; set; }
+    }
+}
