9feb9b8e3caf000dfea2195cafb3d667d9364c31
Carlos Cruz <1701062@sal.ipg.pt>
2021-12-08 13:33:08 +0000

    Criada a classe para representar a ligação entre as tabelas colaborador e projetoSprintDesign (Colaborador_SprintDesign)

---
+++ GestorDeTarefas/Models/Colaborador_SprintDesign.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.Models
+{
+    public class Colaborador_SprintDesign
+    {
+        public int ColaboradorId { get; set; }
+        public Colaborador Colaborador { get; set; }
+
+        public int ID_P_Design { get; set; }
+        public ProjetoSprintDesign ProjetoSprintDesign { get; set; }
+
+        [Display(Name = "Data Inicio")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Por favor, insira a data de inicio do projeto")]
+        public DateTime DataInicio { get; set; }
+
+
+        [Display(Name = "Data Fim")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Por favor, insira a data do fim do projeto")]
+        public DateTime DataFim { get; set; }
+    }
+}
