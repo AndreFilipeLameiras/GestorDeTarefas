00382039894863c73a5ccad98591c366a349c0d0
Carlos Cruz <1701062@sal.ipg.pt>
2021-12-08 13:13:41 +0000

    Adicionada nova migração projeto sprint design

---
+++ GestorDeTarefas/Data/GestorDeTarefasMigrations/20211208130547_P_Sprint_Design.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestorDeTarefas.Data.GestorDeTarefasMigrations
+{
+    public partial class P_Sprint_Design : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProjetoSprintDesign",
+                columns: table => new
+                {
+                    ID_P_Design = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeProjeto = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjetoSprintDesign", x => x.ID_P_Design);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjetoSprintDesign");
+        }
+    }
+}
