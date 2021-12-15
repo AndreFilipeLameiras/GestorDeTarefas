57b32b62a8dfc333edc00318eea7dbd17fd6b1d7
Lameiras <afplameiras@gmail.com>
2021-12-15 22:48:18 +0000

    Criada tabela de ColaboradorIdioma

---
+++ GestorDeTarefas/Data/GestorDeTarefasMigrations/20211215184739_initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestorDeTarefas.Data.GestorDeTarefasMigrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Idioma",
+                columns: table => new
+                {
+                    IdiomaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeIdioma = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Idioma", x => x.IdiomaId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Idioma");
+        }
+    }
+}
---
+++ GestorDeTarefas/Models/ColaboradorIdioma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.Models
+{
+    public class ColaboradorIdioma
+    {
+        public int ColaboradorId { get; set; }
+        public Colaborador Colaborador { get; set; }
+
+
+        public int IdiomaId { get; set; }
+        public Idioma Idioma { get; set; }
+
+
+    }
+}
