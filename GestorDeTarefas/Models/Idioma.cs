064a8743da5396ce319199846d9dcbb31e939b98
Lameiras <afplameiras@gmail.com>
2021-12-15 18:39:19 +0000

    Criaçao do model idioma

---
+++ GestorDeTarefas/Models/Idioma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.Models
+{
+    public class Idioma
+    {
+        public int IdiomaId { get; set; }
+
+        public string NomeIdioma { get; set; }
+
+
+
+    }
+}
