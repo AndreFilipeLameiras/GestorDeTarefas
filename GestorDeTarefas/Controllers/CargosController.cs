956ae62185c4156079c8e0dd9308b68905f498be
João Santos <joaoo.santoss.98@gmail.com>
2021-12-17 11:23:59 +0000

    Funcionalidade de exclusão de cargos alterada

--- GestorDeTarefas/Controllers/CargosController.cs
+++ GestorDeTarefas/Controllers/CargosController.cs
@@ -148,7 +148,10 @@ namespace GestorDeTarefas.Controllers
             var cargo = await _context.Cargo.FindAsync(id);
             _context.Cargo.Remove(cargo);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            ViewBag.Title = "Cargo Apagado";
+            ViewBag.Message = "Cargo apagado com sucesso.";
+            return View("Success");
         }
 
         private bool CargoExists(int id)
