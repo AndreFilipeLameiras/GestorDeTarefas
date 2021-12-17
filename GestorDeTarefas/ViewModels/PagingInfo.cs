db5c92b4b6e571fc8c59470b920e7e2b58149df2
João Santos <joaoo.santoss.98@gmail.com>
2021-12-17 12:24:15 +0000

    Mostra apenas um subconjunto de páginas

--- GestorDeTarefas/ViewModels/PagingInfo.cs
+++ GestorDeTarefas/ViewModels/PagingInfo.cs
@@ -8,7 +8,7 @@ namespace GestorDeTarefas.ViewModels
     public class PagingInfo
     {
 
-		public const int NUMBER_PAGES_SHOW_BEFORE_AFTER = 5;
+		public const int NUMBER_PAGES_SHOW_BEFORE_AFTER = 3;
 
 		public int TotalItems { get; set; }
 		public int PageSize { get; set; } = 4;
