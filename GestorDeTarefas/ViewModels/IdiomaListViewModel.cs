238a643fc6d45f85acee20d0b833e4c8cdf24812
Lameiras <afplameiras@gmail.com>
2021-12-17 18:34:09 +0000

    Criaçao view model para mostrar lista de Idiomas

---
+++ GestorDeTarefas/ViewModels/IdiomaListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GestorDeTarefas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.ViewModels
+{
+    public class IdiomaListViewModel
+    {
+        public IEnumerable<Idioma> Idiomas { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
