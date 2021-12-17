7db59384389fbff3698ffbff99ba9eb8dc29cc68
Carlos Cruz <1701062@sal.ipg.pt>
2021-12-17 02:53:01 +0000

    Criado a classe ProjetoSprintListView

---
+++ GestorDeTarefas/ViewModels/ProjetoSprintListView.cs
@@ -0,0 +1,14 @@
+﻿using GestorDeTarefas.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.ViewModels
+{
+    public class ProjetoSprintListView
+    {
+        public IEnumerable<ProjetoSprint> ProjetoSprints { get; set; } 
+        public PagingInfo PagingInfo { get; set; } 
+    }
+}
