0c1897b28f8854e9f61764ee9865152072e344cd
Carlos Cruz <1701062@sal.ipg.pt>
2021-12-20 16:13:55 +0000

    Adicionado mais campos dentro da tabela projeto Sprint design

--- GestorDeTarefas/Models/Tarefas.cs
+++ GestorDeTarefas/Models/Tarefas.cs
@@ -20,28 +20,29 @@ namespace GestorDeTarefas.Models
 
         [DataType(DataType.Date)]
         [Display(Name = "Data Prevista de Inicio")]
-        [DisplayFormat(DataFormatString = "{dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         [Required(ErrorMessage = "Por favor, insira a data prevista de inicio da tarefa")]
         public DateTime DataPrevistaInicio { get; set; }
 
         [DataType(DataType.Date)]
         [Display(Name = "Data Definitiva do Inicio")]
-        [DisplayFormat(DataFormatString = "{dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         [Required(ErrorMessage = "Por favor, insira a data definitiva de inicio da tarefa")]
         public DateTime DataDefinitivaInicio { get; set; }
 
         [DataType(DataType.Date)]
         [Display(Name = "Data Prevista do Fim")]
-        [DisplayFormat(DataFormatString = "{dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         [Required(ErrorMessage = "Por favor, insira a data prevista do fim da tarefa")]
         public DateTime DataPrevistaFim { get; set; }
 
 
         [DataType(DataType.Date)]
         [Display(Name = "Data Definitiva do Fim")]
-        [DisplayFormat(DataFormatString = "{dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime DataDefinitivaFim { get; set; }
 
+       
         [ForeignKey("FK_ColaboradorId")]
         [DisplayName("Colaborador")]
         public int ColaboradorId { get; set; }
