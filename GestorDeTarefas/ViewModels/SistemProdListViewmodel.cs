8ea4f3ff1a2126600803e333a2a0e1dd8691335a
Lameiras <afplameiras@gmail.com>
2021-12-20 20:51:08 +0000

    Criado modelo de visualização para mostrar a lista de projetos do modelo SistemaProdutividade

---
+++ GestorDeTarefas/ViewModels/SistemProdListViewmodel.cs
@@ -0,0 +1,14 @@
+﻿using GestorDeTarefas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.ViewModels
+{
+    public class SistemProdListViewmodel
+    {
+        public IEnumerable<SistemaProdutividade> ProjetoProdutividade { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
