dc07ead4cfcb0d9127e060316af1f5c5af4f7dac
Carlos Cruz <1701062@sal.ipg.pt>
2021-12-21 01:03:27 +0000

    Adicionado mais campos dentro do ProjetoSprintListViewModel

--- GestorDeTarefas/ViewModels/ProjetoSprintListViewModel.cs
+++ GestorDeTarefas/ViewModels/ProjetoSprintListViewModel.cs
@@ -7,10 +7,14 @@ using System.Threading.Tasks;
 
 namespace GestorDeTarefas.ViewModels
 {
-    public class ProjetoSprintListViewModel
+    public class ProjetoSprintListViewModel 
     {
         public IEnumerable<ProjetoSprintDesign> ProjetoSprints { get; set; } 
         public PagingInfo PagingInfo { get; set; }
         public string NomeSearched { get; set; }
+
+        public int ID_P_Design { get; set; }
+        public string NomeProjeto { get; set; }
+        public List<CheckBoxViewModel>Colaboradores { get; set; }
     }
 }
