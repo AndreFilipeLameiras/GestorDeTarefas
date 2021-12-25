23eccc676f297e5923fe0407b818b16e05a12bbf
Carlos Cruz <1701062@sal.ipg.pt>
2021-12-25 10:24:41 +0000

    Criado o modelo estado do projeto

---
+++ GestorDeTarefas/Models/EstadoProjeto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.Models
+{
+    public class EstadoProjeto
+    {
+        [Key]
+        public int Id_Estado { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Por favor, insira o estado")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "O estado deve ter entre 5 e 20 caracteres")]
+        public string NomeEstado { get; set; }
+    }
+}
