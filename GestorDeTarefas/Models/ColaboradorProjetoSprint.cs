d99ec0942df77b37f2cf941eccdfb9ff4a0954ea
Carlos Cruz <1701062@sal.ipg.pt>
2021-12-25 12:07:03 +0000

    Adicionado as datas dos colaboradores no projetoSptint

--- GestorDeTarefas/Models/ColaboradorProjetoSprint.cs
+++ GestorDeTarefas/Models/ColaboradorProjetoSprint.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,19 @@ namespace GestorDeTarefas.Models
 
         public int ColaboradorId { get; set; }
         public Colaborador Colaborador { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Inicio")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [Required(ErrorMessage = "Por favor, insira a data de inicio do colaborador no projeto")]
+        public DateTime DataInicio { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Fim")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [Required(ErrorMessage = "Por favor, insira a data de fim do colaborador no projeto")]
+        public DateTime DataFim { get; set; }
+
     }
 
 }
