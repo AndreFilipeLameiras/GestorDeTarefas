9bc0c7cf7d3cb564b638758ffe272eefee8abe19
Carlos Cruz <1701062@sal.ipg.pt>
2022-01-02 12:36:47 +0000

    Adicionado as datas de inicio e fim dos colaboradores na view do projeto sprint

--- GestorDeTarefas/ViewModels/CheckBoxViewModel.cs
+++ GestorDeTarefas/ViewModels/CheckBoxViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using GestorDeTarefas.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,12 @@ namespace GestorDeTarefas.ViewModels
         public int Id { get; set; }
         public string Name { get; set; }
         public bool Checked { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DataInicio { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DataFim { get; set; }
 
         public ColaboradorProjetoSprint ColaboradorProjetoSprintss { get; set; }
     }
