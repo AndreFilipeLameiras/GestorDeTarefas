cf862c7ecf0ae241a06303b21ad1e2dd77b21a02
Carlos Cruz <carlossocegadocruz@gmail.com>
2022-01-11 17:20:16 +0000

    Scaffolding contacto

---
+++ GestorDeTarefas/Controllers/ContactoesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GestorDeTarefas.Data;
+using GestorDeTarefas.Models;
+
+namespace GestorDeTarefas.Controllers
+{
+    public class ContactoesController : Controller
+    {
+        private readonly GestorDeTarefasContext _context;
+
+        public ContactoesController(GestorDeTarefasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contactoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Contacto.ToListAsync());
+        }
+
+        // GET: Contactoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = await _context.Contacto
+                .FirstOrDefaultAsync(m => m.ContactoId == id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        // GET: Contactoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contactoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ContactoId,Nome,Sobrenome,Email,Assunto,Mensagem")] Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contacto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contacto);
+        }
+
+        // GET: Contactoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = await _context.Contacto.FindAsync(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+            return View(contacto);
+        }
+
+        // POST: Contactoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ContactoId,Nome,Sobrenome,Email,Assunto,Mensagem")] Contacto contacto)
+        {
+            if (id != contacto.ContactoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contacto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactoExists(contacto.ContactoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contacto);
+        }
+
+        // GET: Contactoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = await _context.Contacto
+                .FirstOrDefaultAsync(m => m.ContactoId == id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        // POST: Contactoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contacto = await _context.Contacto.FindAsync(id);
+            _context.Contacto.Remove(contacto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContactoExists(int id)
+        {
+            return _context.Contacto.Any(e => e.ContactoId == id);
+        }
+    }
+}
