d32b6552c0ea4d7dc5e46dc609ee48946ac7b5d9
Carlos Cruz <carlossocegadocruz@gmail.com>
2022-01-11 17:18:22 +0000

    Crado o model contacto

---
+++ GestorDeTarefas/Models/Contacto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.Models
+{
+    public class Contacto
+    {
+        public int ContactoId { get; set; }
+
+        [Required(ErrorMessage = "Por favor, insira seu Nome")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "O nome deve ter pelo menos 2 caracteres e um máximo de 20")]
+        public string Nome { get; set; }
+
+
+        [Required(ErrorMessage = "Por favor, insira seu Sobrenome")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "O Sobrenome deve ter pelo menos 2 caracteres e um máximo de 20")]
+        public string Sobrenome { get; set; }
+
+
+
+        [Required(ErrorMessage = "Por favor, introduza o email")]
+        [EmailAddress(ErrorMessage = "Por favor digite um email válido")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Por favor, escreva o assunto")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Assunto deve ter pelo menos 4 caracteres e um máximo de 20")]
+        public string Assunto { get; set; }
+
+        [Required(ErrorMessage = "Por favor, escreva a mensagem")]
+        [StringLength(1000, MinimumLength = 2, ErrorMessage = "Mensagem deve ter pelo menos 2 caracteres e um máximo de 1000")]
+        public string Mensagem { get; set; }
+    }
+}
