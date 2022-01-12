e7ebf4aa4b70d7b232c2a57df5ed4e85880f0025
João Santos <joaoo.santoss.98@gmail.com>
2022-01-12 19:53:48 +0000

    Scaffold identity

---
+++ GestorDeTarefas/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GestorDeTarefas.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GestorDeTarefas.Areas.Identity.IdentityHostingStartup))]
+namespace GestorDeTarefas.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
