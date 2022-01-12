0161cb49d646e9db9e16aebeeee062523d7ac658
Lameiras <afplameiras@gmail.com>
2022-01-12 15:44:17 +0000

    Alteraçao na pagina de Registar Cliente para ler password

--- GestorDeTarefas/ViewModels/RegistarClienteViewModel.cs
+++ GestorDeTarefas/ViewModels/RegistarClienteViewModel.cs
@@ -25,9 +25,13 @@ namespace GestorDeTarefas.ViewModels
         public string Phone { get; set; }
 
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "As PassWords não correspondem")]
         public string ConfirmPassword { get; set; }
     }
