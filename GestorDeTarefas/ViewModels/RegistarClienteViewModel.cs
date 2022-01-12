7bb2ff4c53994096795bef807e60ef845ce4df66
Lameiras <afplameiras@gmail.com>
2022-01-12 15:26:37 +0000

    Adicionar view model para registar Clientes

---
+++ GestorDeTarefas/ViewModels/RegistarClienteViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.ViewModels
+{
+    public class RegistarClienteViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Cidade { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "As PassWords não correspondem")]
+        public string ConfirmPassword { get; set; }
+    }
+}
