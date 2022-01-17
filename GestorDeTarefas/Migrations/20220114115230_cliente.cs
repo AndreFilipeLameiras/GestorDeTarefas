39ffbf5314a4ce4041726049a5606ec80a4e8948
Lameiras <afplameiras@gmail.com>
2022-01-17 14:54:52 +0000

    Adicionada migraçao nova

---
+++ GestorDeTarefas/Migrations/20220114115230_cliente.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestorDeTarefas.Migrations
+{
+    public partial class cliente : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Morada",
+                table: "Cliente",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Morada",
+                table: "Cliente");
+        }
+    }
+}
