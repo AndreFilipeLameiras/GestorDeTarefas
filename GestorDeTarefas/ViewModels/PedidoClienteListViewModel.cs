ba5b48326077566f25804a781ff108d43a3c3a74
Carlos Cruz <1701062@sal.ipg.pt>
2022-01-18 02:23:58 +0000

    Algumas alterações no Pedido do client list view model

--- GestorDeTarefas/ViewModels/PedidoClienteListViewModel.cs
+++ GestorDeTarefas/ViewModels/PedidoClienteListViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using GestorDeTarefas.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +12,19 @@ namespace GestorDeTarefas.ViewModels
     {
         public IEnumerable<PedidoCliente> PedidoCliente { get; set; }
 
+        public IEnumerable<SelectListItem> ProjetoSprintDesign { get; set; }
+        public IEnumerable<SelectListItem> SistemaProdutividade { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Data para realização do pedido")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime DataRealizarPedido { get; set; }
+        public int ProjetoSprintDesignID { get; set; }
+
+        public int? SistemaProdutividadeId { get; set; }
         public int ClienteId { get; set; }
 
+        
         public string Nome { get; set; }
 
         public string Email { get; set; }
