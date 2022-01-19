cc0876416833fbd2d46fb27fd62ecbbb281d6b9b
Lameiras <afplameiras@gmail.com>
2022-01-19 11:05:58 +0000

    Adicionada nova migraçao

--- GestorDeTarefas/Migrations/20220119105000_prioridade.Designer.cs
+++ GestorDeTarefas/Migrations/20220119105000_prioridade.Designer.cs
@@ -4,14 +4,16 @@ using GestorDeTarefas.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GestorDeTarefas.Migrations
 {
     [DbContext(typeof(GestorDeTarefasContext))]
-    partial class GestorDeTarefasContextModelSnapshot : ModelSnapshot
+    [Migration("20220119105000_prioridade")]
+    partial class prioridade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
