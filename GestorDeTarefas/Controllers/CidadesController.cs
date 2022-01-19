eec33c0507a0cff22a528529f29b0fa8ec3f051b
João Santos <joaoo.santoss.98@gmail.com>
2022-01-19 09:22:30 +0000

    Scafolding das cidades

---
+++ GestorDeTarefas/Controllers/CidadesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GestorDeTarefas.Data;
+using GestorDeTarefas.Models;
+
+namespace GestorDeTarefas.Controllers
+{
+    public class CidadesController : Controller
+    {
+        private readonly GestorDeTarefasContext _context;
+
+        public CidadesController(GestorDeTarefasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cidades
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cidade.ToListAsync());
+        }
+
+        // GET: Cidades/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cidade = await _context.Cidade
+                .FirstOrDefaultAsync(m => m.CidadeId == id);
+            if (cidade == null)
+            {
+                return NotFound();
+            }
+
+            return View(cidade);
+        }
+
+        // GET: Cidades/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cidades/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CidadeId,Nome_Cidade")] Cidade cidade)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cidade);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cidade);
+        }
+
+        // GET: Cidades/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cidade = await _context.Cidade.FindAsync(id);
+            if (cidade == null)
+            {
+                return NotFound();
+            }
+            return View(cidade);
+        }
+
+        // POST: Cidades/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CidadeId,Nome_Cidade")] Cidade cidade)
+        {
+            if (id != cidade.CidadeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cidade);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CidadeExists(cidade.CidadeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cidade);
+        }
+
+        // GET: Cidades/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cidade = await _context.Cidade
+                .FirstOrDefaultAsync(m => m.CidadeId == id);
+            if (cidade == null)
+            {
+                return NotFound();
+            }
+
+            return View(cidade);
+        }
+
+        // POST: Cidades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cidade = await _context.Cidade.FindAsync(id);
+            _context.Cidade.Remove(cidade);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CidadeExists(int id)
+        {
+            return _context.Cidade.Any(e => e.CidadeId == id);
+        }
+    }
+}
