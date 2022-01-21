35d8350ae7edc6c1b0454b18265f6dc5c72ab849
Carlos Cruz <carlossocegadocruz@gmail.com>
2022-01-21 16:06:58 +0000

    Adicionado nova migração

---
+++ GestorDeTarefas/Migrations/20220121155012_pedidoV5.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestorDeTarefas.Migrations
+{
+    public partial class pedidoV5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ColaboradorId",
+                table: "PedidoCliente",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ColaboradorId",
+                table: "PedidoCliente",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
