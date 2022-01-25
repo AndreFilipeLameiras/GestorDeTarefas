1e146af7ce84c29083ad19f5886d9822d5aa5d30
Carlos Cruz <1701062@sal.ipg.pt>
2022-01-25 07:16:29 +0000

    Adicionado nova migração

---
+++ GestorDeTarefas/Migrations/20220125061158_3rd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestorDeTarefas.Migrations
+{
+    public partial class _3rd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataPedido",
+                table: "PedidoCliente",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataPedido",
+                table: "PedidoCliente",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
