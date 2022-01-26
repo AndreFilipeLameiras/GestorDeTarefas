10a5af6a0e75adbb40634192961e6664eb7fe3ea
Lameiras <afplameiras@gmail.com>
2022-01-26 10:08:15 +0000

    Criaçao de editar o cliente ja logado

---
+++ GestorDeTarefas/Models/EditarClienteRegistadoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.Models
+{
+    public class EditarClienteRegistadoViewModel
+    {
+        [Required]
+        [StringLength(128)]
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
