5412c741f84109e1f7029187617f7d2f39520163
João Santos <joaoo.santoss.98@gmail.com>
2022-01-26 17:43:53 +0000

    Criado viewmodel mostraridioma

---
+++ GestorDeTarefas/ViewModels/MostrarIdiomaListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GestorDeTarefas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorDeTarefas.ViewModels
+{
+    public class MostrarIdiomaListViewModel
+    {
+        public IEnumerable<Colaborador> Colaborador { get; set; }
+
+        public IEnumerable<ColaboradorIdioma> ColaboradorIdioma { get; set; }
+    }
+}
